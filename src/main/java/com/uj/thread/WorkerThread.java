5de2fbb0fa015fb6eb85811708dad44a8a983575
upendra <joshiupendra1995>
2020-04-18 08:25:52 +0530

    multithreading example with springboot

---
+++ src/main/java/com/uj/thread/WorkerThread.java
@@ -0,0 +1,48 @@
+package com.uj.thread;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.concurrent.Callable;
+
+// We need a scope of prototype so that a new instance is created each time. The default scope is singleton so you would always get the same instance
+// if we did not specify a scope of prototype.
+
+@Component
+@Scope("prototype")
+public class WorkerThread implements Callable<String> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkerThread.class);
+
+    private String request;
+
+    public WorkerThread(String request) {
+        this.request = request;
+    }
+
+    @Override
+    public String call() throws Exception {
+        LOGGER.debug("Thread started:{} " , request);
+        return doWork();
+    }
+
+    private String doWork() throws InterruptedException {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            LOGGER.error("An unexpected interrupt exception occurred!", e);
+            throw e;
+        }
+
+        return "Request [" + request + "] " + createUUID();
+    }
+
+    private String createUUID() {
+        UUID id = UUID.randomUUID();
+        return id.toString();
+    }
+
+}
